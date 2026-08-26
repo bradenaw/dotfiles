82a95d18457bc3e0d87a49724a3908584f9386a2
Braden Walker <bradenawalker@gmail.com>
2026-08-26 00:57:21 -0400

    install lazy with a submodule instead of the weird bootstrap

--- base/.config/nvim/lua/init.lua
+++ base/.config/nvim/lua/init.lua
@@ -1,23 +1,6 @@
 vim.g.mapleader = ","
 
--- Pasted from lazy.folke.io/installation ----------------------------------------------------------
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.fn.stdpath("config") .. "/lazy.nvim")
 
 -- Setup lazy.nvim
 require("lazy").setup({
