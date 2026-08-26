b44a0a21921e13a2dbdae7c7307e24e69f78e1ce
Braden Walker <bradenawalker@gmail.com>
2026-08-26 01:19:32 -0400

    split plugins also

---
+++ base/.config/nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,15 @@
+return {
+    'luukvbaal/statuscol.nvim',
+    config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+            setopt = true,
+            segments = {
+                { sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true }, },
+                { sign = { namespace = { "diagnostic" }, maxwidth = 1, colwidth = 1, auto = true }, },
+                { text = { builtin.lnumfunc } },
+                { text = { " " } },
+            },
+        })
+    end,
+}
